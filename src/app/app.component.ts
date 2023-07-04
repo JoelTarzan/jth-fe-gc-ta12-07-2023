2f30d9fb7d48d2f357348feb490f873c9a54bdaf
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2023-07-04 17:15:56 +0200

    Creacion del proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'jth-fe-gc-ta12-07-2023';
+}
